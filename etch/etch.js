bb18f91db267eeafe715c7b9f21d4766d5b4b40f
Will Robertson <williamdrobertson42@gmail.com>
2021-08-22 19:02:56 +1000

    reset button

--- etch/etch.js
+++ etch/etch.js
@@ -1,13 +1,29 @@
 const container = document.getElementById("container")
+const resetButton = document.getElementById("reset")
 
-
-function createGrid(size){
+function createGrid(size) {
 	for (let i = 0; i < size * size; i++) {
-	const gridSquare = document.createElement('div')
-	gridSquare.setAttribute('class', 'gridSquare')
-	container.appendChild(gridSquare)
-	console.log("Square created.")		
+		const gridSquare = document.createElement('div')
+		gridSquare.setAttribute('class', 'gridSquare')
+		container.appendChild(gridSquare)
+		console.log("Square created.")
+		gridSquare.addEventListener("mouseover", (e) => 
+		changeColour(e, this)
+		);
 	}
 }
+
+function resetGrid() {
+	const gridChildren = container.children
+	for (const element of gridChildren) {
+		element.style.backgroundColor = "white"
+	}
+}
+
+function changeColour(event) {
+	event.target.style.backgroundColor = "black"
+}
+
+resetButton.addEventListener("click",(e) => resetGrid())
 
 createGrid(16)
