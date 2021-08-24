7061e010d6932a5e18996ef106b0f3dc1203ec88
Will Robertson <williamdrobertson42@gmail.com>
2021-08-24 01:27:12 -0700

    Resize button click

--- etch/etch.js
+++ etch/etch.js
@@ -1,14 +1,17 @@
 const container = document.getElementById("container")
 const resetButton = document.getElementById("reset")
-
+const resizeButton = document.getElementById("resize")
+let containerField = document.getElementById("containerSize")
+let root = document.documentElement;
+let containerSize = 0;
 function createGrid(size) {
 	for (let i = 0; i < size * size; i++) {
 		const gridSquare = document.createElement('div')
 		gridSquare.setAttribute('class', 'gridSquare')
 		container.appendChild(gridSquare)
 		console.log("Square created.")
-		gridSquare.addEventListener("mouseover", (e) => 
-		changeColour(e, this)
+		gridSquare.addEventListener("mouseover", (e) =>
+			changeColour(e, this)
 		);
 	}
 }
@@ -24,6 +27,20 @@ function changeColour(event) {
 	event.target.style.backgroundColor = "black"
 }
 
-resetButton.addEventListener("click",(e) => resetGrid())
+function resizeGrid(newSize) {
+	console.log(newSize)
+	container.replaceChildren();
+	root.style.setProperty('--gridsize',`${newSize}`);
+	resetGrid()
+	createGrid(newSize)
+}
 
 createGrid(16)
+
+containerField.addEventListener("keyup", (e) => {
+	containerSize = e.target.value
+	console.log(containerSize)
+});
+
+resetButton.addEventListener("click", (e) => resetGrid())
+resizeButton.addEventListener("click", (e) => resizeGrid(containerSize))
