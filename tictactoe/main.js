0968d0ca56da4a440e6fb2e40ece810e15a2eefb
Will Robertson <williamdrobertson42@gmail.com>
2021-09-30 14:53:40 +1000

    run Prettier on default.

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -1,14 +1,17 @@
 class Gameboard {
-	constructor(boardcells) {
-		this.cellArray = [];
-		for (let index = 0; index < boardcells; index++) {
-		this.cellArray.push(new Gamecell());
-		}
-}
+  constructor(boardcells) {
+    this.cellArray = [];
+    for (let index = 0; index < boardcells; index++) {
+      this.cellArray.push(new Gamecell());
+    }
+  }
 }
 
 class Gamecell {
+  constructor() {
+    this.ticked = 0;
+  }
 }
 
 const board = new Gameboard(9);
-console.log(board)
+console.log(board);
