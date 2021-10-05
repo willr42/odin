0fe824fd2daba47ea0aa09ded4070747de455211
Will Robertson <williamdrobertson42@gmail.com>
2021-10-05 20:38:57 +1100

    add drawCell function

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -2,13 +2,16 @@ class Gameboard {
 	constructor(boardcells) {
 		this.cellArray = [];
 		for (let index = 0; index < boardcells; index++) {
-		this.cellArray.push(new Gamecell());
+			this.cellArray.push({ ticked: 0 });
 		}
-}
+	}
+	drawCell(cell) {
+		const cellDiv = document.createElement("div")
+		cellDiv.textContent('')
+		cellDiv.className = 'blankCell'
+	}
 }
 
-class Gamecell {
-}
 
 const board = new Gameboard(9);
 console.log(board)
