4a75ed3a9ddf7841f20c8a3a85a90acdbd3a6f5d
Will Robertson <williamdrobertson42@gmail.com>
2021-10-10 03:12:03 -0700

    start drawboard function

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -2,16 +2,19 @@ class Gameboard {
   constructor(boardcells) {
     this.cellArray = [];
     for (let index = 0; index < boardcells; index++) {
-      this.cellArray.push(new Gamecell());
+      this.cellArray.push({ ticked: 0 });
     }
   }
-}
 
-class Gamecell {
-  constructor() {
-    this.ticked = 0;
+  drawBoard() {
+    const boardDiv = document.createElement('div')
+    this.cellArray.forEach(element => {
+      this.drawCell(element)
+    });
+  }
+
+  redrawBoard() {
+    //TODO: some sort of redraw function when gamestate changes.
   }
 }
-
 const board = new Gameboard(9);
-console.log(board);
