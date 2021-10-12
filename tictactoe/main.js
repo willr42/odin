6229a2f63d3ebac72b1723341f44c1bc9befd104
Will Robertson <williamdrobertson42@gmail.com>
2021-10-12 02:43:13 -0700

    add createBoard and drawBoard function

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -3,15 +3,29 @@ class Gameboard {
   constructor(boardcells) {
     this.cellArray = [];
     for (let index = 0; index < boardcells; index++) {
-      this.cellArray.push({ ticked: 0 });
+      this.cellArray.push({ ticked: null });
     }
   }
 
-  drawBoard() {
+  createBoard() {
     const boardDiv = document.createElement('div')
+    boardDiv.id = 'gameboard'
     this.cellArray.forEach(element => {
-      this.drawCell(element)
+      const cell = document.createElement('div')
+      cell.className = 'cell'
+      if (element.ticked === "player") {
+        cell.textContent = 'O'
+      } else if (element.ticked === "enemy") {
+        cell.textContent = 'X'
+      }
+      boardDiv.appendChild(cell)
     });
+    return boardDiv
+  }
+  drawBoard() {
+    const boardAndChild = this.createBoard()
+    const container = document.querySelector('#container')
+    container.appendChild(boardAndChild)
   }
 
   redrawBoard() {
@@ -19,3 +33,4 @@ class Gameboard {
   }
 }
 const board = new Gameboard(9);
+board.drawBoard();
