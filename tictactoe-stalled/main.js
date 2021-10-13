d38595349101324213746eaaee42a152df1324e6
Will Robertson <williamdrobertson42@gmail.com>
2021-10-13 03:06:57 -0700

    add checkWin function.

--- tictactoe-stalled/main.js
+++ tictactoe-stalled/main.js
@@ -7,7 +7,7 @@ class Gameboard {
   constructor(boardcells) {
     this.cellArray = [];
     for (let index = 0; index < boardcells; index++) {
-      this.cellArray.push({ ticked: null });
+      this.cellArray.push("");
     }
   }
 
@@ -21,15 +21,35 @@ class Gameboard {
     this.cellArray.forEach(element => {
       const cell = document.createElement('div')
       cell.className = 'cell'
-      if (element.ticked === "player") {
+      if (element === "O") {
         cell.textContent = 'O'
-      } else if (element.ticked === "enemy") {
+      } else if (element === "X") {
         cell.textContent = 'X'
       }
       boardDiv.appendChild(cell)
     });
     return boardDiv
   }
+
+  checkWin(letter) {
+    if (this.cellArray[0] === letter && this.cellArray[1] === letter && this.cellArray[2] === letter // horizontals
+      || this.cellArray[3] === letter && this.cellArray[4] === letter && this.cellArray[5] === letter
+      || this.cellArray[6] === letter && this.cellArray[7] === letter && this.cellArray[8] === letter
+      || this.cellArray[0] === letter && this.cellArray[3] === letter && this.cellArray[6] === letter // verticals
+      || this.cellArray[1] === letter && this.cellArray[4] === letter && this.cellArray[7] === letter
+      || this.cellArray[2] === letter && this.cellArray[5] === letter && this.cellArray[8] === letter
+      || this.cellArray[0] === letter && this.cellArray[4] === letter && this.cellArray[5] === letter // diagonals
+      || this.cellArray[2] === letter && this.cellArray[4] === letter && this.cellArray[6] === letter
+    ) {
+      return true
+    }
+  }
+
+  move(event) {
+ // implement playermove. Need to know which player's turn it is and which symbol they're using... 
+    this.cellArray
+  }
+
   drawBoard() {
     const boardAndChild = this.createBoard()
     const container = document.querySelector('#container')
