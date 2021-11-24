2eef45b0858f841349afa928dfd53a97681b1557
Will Robertson <williamdrobertson42@gmail.com>
2021-11-24 15:35:18 +1100

    Set webpack mode

--- restaurant/webpack.config.js
+++ restaurant/webpack.config.js
@@ -1,9 +1,11 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
+
