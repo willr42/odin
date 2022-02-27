a8ebee1b0a117a2d2f9f4fc854fb3b038a21d00d
Will Robertson <williamdrobertson42@gmail.com>
2022-02-27 22:00:34 +1100

    basic styles for homepage

--- restaurant/src/home.js
+++ restaurant/src/home.js
@@ -8,8 +8,6 @@ function drawHomepage() {
   title.textContent = "Welcome to Applebeys";
   description.textContent = "Pepper jack emmental cheese strings. Cheese strings queso brie cheese triangles danish fontina everyone loves fromage say cheese. Babybel blue castello cauliflower cheese dolcelatte st. agur blue cheese cheese on toast dolcelatte cottage cheese. Melted cheese goat jarlsberg cheesecake cheese strings stinking bishop."
   image.src = applebees;
-  image.width = 640;
-  image.height = 480;
   const content = [title, image, description]
   content.forEach(element => {
     rootDiv.appendChild(element)
