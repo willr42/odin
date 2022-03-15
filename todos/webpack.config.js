1aa6c8b96257207bc7eade4f5e7ca0db9bcb79b4
Will Robertson <williamdrobertson42@gmail.com>
2022-03-15 00:27:04 -0700

    Remove webpack HTML bundler.
    Am using my own written HTML file for this project.

--- todos/webpack.config.js
+++ todos/webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   entry: './src/index.js',
@@ -25,13 +24,7 @@ const config = {
         ]
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
-      filename: 'index.html',
-    })
-  ]
+  }
 };
 
 module.exports = config;
