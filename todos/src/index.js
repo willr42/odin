5b57f1d731213433814095bc9f28e90ce16b34f8
Will Robertson <williamdrobertson42@gmail.com>
2022-03-16 02:17:19 -0700

    Build form HTML

--- todos/src/index.js
+++ todos/src/index.js
@@ -1,1 +1,11 @@
 import Todo from "./Todo.js"
+
+const todoArea = document.querySelector("#todo-area")
+
+// collect input for new todo
+
+const form = document.getElementById("newTodo")
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+})
