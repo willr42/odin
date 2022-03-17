83a4d1207c83be91afcf28e2f82e1911d9f7f11a
Will Robertson <williamdrobertson42@gmail.com>
2022-03-17 21:48:08 +1100

    Validation of strings

--- todos/src/index.js
+++ todos/src/index.js
@@ -1,6 +1,9 @@
+import { validate } from "webpack/node_modules/schema-utils"
 import Todo from "./Todo.js"
 
 const todoArea = document.querySelector("#todo-area")
+
+// Create new todo button
 const newTodoButton = document.createElement("button")
 newTodoButton.classList.add("newTodoButton")
 newTodoButton.textContent = "+"
@@ -11,6 +14,25 @@ todoArea.appendChild(newTodoButton)
 
 const form = document.getElementById("newTodo")
 
+function validateString(formElement) {
+	console.log("formElement value:", formElement)
+	if (formElement === "") {
+		console.log("No formElement value, returning false.")
+		return false;
+	} else {
+console.log("Returning ture.")
+return true
+}}
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault()
+	const formTitle = form.title.value
+	const formDate = form.duedate.value
+	const formDescription = form.description.value
+	if (!validateString(formTitle)) {
+	console.log("There is no form title.")
+	} else {
+		console.log("There is a form title.")
+	}
+	// console.table([{"form": "formTitle", "value": formTitle},{"form": "formDate", "value": formDate},{"form":"formDescription", "value":formDescription}])
 })
