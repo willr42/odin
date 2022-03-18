10ac3d57d58df08344893c961916aff5b1e8c527
Will Robertson <williamdrobertson42@gmail.com>
2022-03-18 05:14:46 -0700

    Rename popup to modal & refactor modal JS

--- todos/src/components/NewTodoButton.js
+++ todos/src/components/NewTodoButton.js
@@ -1,17 +1,10 @@
 function NewTodoButton() {
 	const todoArea = document.querySelector("#todo-area");
 	const newTodoButton = document.createElement("button");
-	const popup = document.querySelector("#popup-background")
+	const popup = document.querySelector("#modal")
+	const popupMenu = document.querySelector("#modal-content")
 	newTodoButton.classList.add("newTodoButton");
 	newTodoButton.textContent = "+";
-	newTodoButton.addEventListener("click", (e) => {
-		console.log("newTodoButton clicked. Event", e);
-		popup.classList.remove("hidden")
-		popup.addEventListener("click", () => {
-			popup.classList.add("hidden")
-		})
-		// This needs to open the modal to add a new Todo.
-	});
 	todoArea.appendChild(newTodoButton);
 }
 
