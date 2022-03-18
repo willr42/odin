2c4eb6915e566f335196c3b45c9a42579d790f77
Will Robertson <williamdrobertson42@gmail.com>
2022-03-18 01:38:48 -0700

    basic modal toggle

--- todos/src/components/NewTodoButton.js
+++ todos/src/components/NewTodoButton.js
@@ -1,13 +1,18 @@
 function NewTodoButton() {
-  const todoArea = document.querySelector("#todo-area");
-  const newTodoButton = document.createElement("button");
-  newTodoButton.classList.add("newTodoButton");
-  newTodoButton.textContent = "+";
-  newTodoButton.addEventListener("click", (e) => {
-    console.log("newTodoButton clicked. Event", e);
-    // This needs to open the modal to add a new Todo.
-  });
-  todoArea.appendChild(newTodoButton);
+	const todoArea = document.querySelector("#todo-area");
+	const newTodoButton = document.createElement("button");
+	const popup = document.querySelector("#popup-background")
+	newTodoButton.classList.add("newTodoButton");
+	newTodoButton.textContent = "+";
+	newTodoButton.addEventListener("click", (e) => {
+		console.log("newTodoButton clicked. Event", e);
+		popup.classList.remove("hidden")
+		popup.addEventListener("click", () => {
+			popup.classList.add("hidden")
+		})
+		// This needs to open the modal to add a new Todo.
+	});
+	todoArea.appendChild(newTodoButton);
 }
 
 export default NewTodoButton
