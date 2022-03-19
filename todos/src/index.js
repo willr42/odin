67041427418e5740e3b23e895b4a9631bf683ce9
Will Robertson <williamdrobertson42@gmail.com>
2022-03-19 04:41:59 -0700

    Initial adding todos behaviour

--- todos/src/index.js
+++ todos/src/index.js
@@ -1,11 +1,28 @@
-import Modal from "./components/modal.js"
-import Todo from "./components/Todo.js"
-Modal()
-const form = document.getElementById("newTodo")
+import Modal from "./components/modal.js";
+import Todo from "./components/Todo.js";
+const lists = [];
+let defaultList = {
+	listname: "default",
+	todos: []
+};
+lists.push(defaultList)
+
+Modal();
+const form = document.getElementById("newTodo");
 
 form.addEventListener("submit", (e) => {
-	e.preventDefault()
-	console.log("form title:", form.title.value)
-	const result = new Todo(form.title.value, form.description.value, form.duedate.value)
-	console.log("New Todo:", result)
-})
+  e.preventDefault();
+  console.log("form title:", form.title.value);
+  const result = new Todo(
+    form.title.value,
+    form.description.value,
+    form.duedate.value
+  );
+  defaultList.todos.push(result)
+  console.log("Default List:", defaultList)
+  // remove validation below. Check which list is "open" by looking at classlists of all lists in DOM (also need func to create new lists in DOM.) If there isn't one, it will be default - add to that list and re-render. Otherwise, add to the list that is open.
+  
+});
+
+//TODO: load from & save to localStorage whenever state changes. New List. New Todo. Todo deleted. List deleted.
+//TODO: initialise lists from local storage as options in select dropdown
