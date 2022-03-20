76986d2605acde4f024a8b83fc9c6c5dcbb89c5f
Will Robertson <williamdrobertson42@gmail.com>
2022-03-20 02:43:25 -0700

    Refactor into separate Template function

--- todos/src/components/lists.js
+++ todos/src/components/lists.js
@@ -1,4 +1,5 @@
-import Todo from "./Todo";
+import isEmpty from "../utils/objectIsEmpty"
+
 function addTodoToList(newTodo, lists) {
   const activeListName = document.querySelector(".activeList").textContent
   if (!lists[activeListName]) {
@@ -8,9 +9,6 @@ function addTodoToList(newTodo, lists) {
 
 }
 
-function isEmpty(lists){
-  return Object.keys(lists).length === 0;
-}
 
 // add .activeList class to the list that is being actively displayed. This fires when a list is selected in the sidebar.
 // On first load, this should be set to default.
@@ -18,32 +16,35 @@ function isEmpty(lists){
 
 function renderLists(lists) {
   if (isEmpty(lists)) {
-    let temp = new Todo("test todo", "This is my test todo", "1999-01-01")
     lists.defaultList = []
-    lists.defaultList.push(temp)
   }
+
   for (const list in lists) {
     if (Object.hasOwnProperty.call(lists, list)) {
-      const singleListArray = lists[list];
-      const todoArea = document.querySelector("#todo-area")
-      const template = document.querySelector("#todo-template")
-      for (const listItem of singleListArray) {
+      const oneList = lists[list];
+      for (const listItem of oneList) {
         if (!listItem) {
           console.log("List empty. Finishing early")
           break
         }
-        const clone = template.content.cloneNode(true)
-        const heading = clone.querySelector(".todo-heading")
-        const body = clone.querySelector(".todo-content")
-        console.log("heading is: ", heading, "body is:", body)
-        heading.textContent = listItem.title;
-        body.textContent = listItem.description;
-        todoArea.appendChild(clone)
-        console.log("Clone appended:", clone)
+        attachTemplate(listItem)
       }
     }
   }
 }
 
+function attachTemplate(listItem) {
+  const todoArea = document.querySelector("#todo-area")
+  const template = document.querySelector("#todo-template")
+  const clone = template.content.cloneNode(true)
+  const heading = clone.querySelector(".todo-heading")
+  const body = clone.querySelector(".todo-content")
+  console.log("heading is: ", heading, "body is:", body)
+  heading.textContent = listItem.title;
+  body.textContent = listItem.description;
+  todoArea.appendChild(clone)
+  console.log("Clone appended:", clone)
+
+}
 
 export { addTodoToList, renderLists }
