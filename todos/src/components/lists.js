c14bd41b31ea329842eeae241feed48941b012c1
Will Robertson <williamdrobertson42@gmail.com>
2022-03-24 14:50:11 +1100

    Separate list input validation

--- todos/src/components/lists.js
+++ todos/src/components/lists.js
@@ -6,21 +6,7 @@ function addTodoToList(newTodo, lists) {
 }
 
 function createList(lists) {
-  // shift this validation logic into another function so we're not continually creating an error Element we then throw away?
-  const listInput = document.getElementById("list_name")
-  const addListContainer = document.querySelector(".addList-container")
-  if (listInput.value === "" & !addListContainer.classList.contains("openError")) {
-    const errorElement = document.createElement("p")
-    errorElement.textContent = "HEY! No empty bukkits."
-    errorElement.style.color = "red"
-    addListContainer.classList.add("openError")
-    addListContainer.appendChild(errorElement)
-    listInput.focus()
-    return
-  } else if (listInput.value === "" & addListContainer.classList.contains("openError")) {
-    listInput.focus()
-    return
-  } else {
+  if (listInputIsValid()) {
     if (addListContainer.classList.contains("openError")) {
       const existingErrorElement = document.querySelector("#newList > p")
       console.log("removing error element:", existingErrorElement)
@@ -30,6 +16,24 @@ function createList(lists) {
     lists[listInput.value] = []
     addNewListToDOM(listInput.value)
   }
+}
+
+function listInputIsValid() {
+  const listInput = document.getElementById("list_name")
+  const addListContainer = document.querySelector(".addList-container")
+  if (listInput.value === "" & !addListContainer.classList.contains("openError")) {
+    const errorElement = document.createElement("p")
+    errorElement.textContent = "HEY! No empty bukkits."
+    errorElement.style.color = "red"
+    addListContainer.classList.add("openError")
+    addListContainer.appendChild(errorElement)
+    listInput.focus()
+    return false
+  } else if (listInput.value === "" & addListContainer.classList.contains("openError")) {
+    listInput.focus()
+    return false
+  }
+  return true;
 }
 
 function addNewListToDOM(listName) {
@@ -51,8 +55,8 @@ function changeActiveList(e, globalLists) {
       } else {
         child.classList.add("activeList")
         renderList(e.target.textContent, globalLists)
-      const newTodoButton = document.querySelector(".newTodoButton")
-      newTodoButton.classList.remove("hidden")
+        const newTodoButton = document.querySelector(".newTodoButton")
+        newTodoButton.classList.remove("hidden")
       }
     }
   }
@@ -62,7 +66,7 @@ function renderList(selectedList, globalLists) {
   const todoArea = document.querySelector("#todo-area")
   const newTodoButton = document.querySelector(".newTodoButton")
   while (todoArea.lastChild !== newTodoButton) {
-    todoArea.removeChild(todoArea.lastChild)    
+    todoArea.removeChild(todoArea.lastChild)
   }
   renderTodosInList(globalLists[selectedList])
 }
