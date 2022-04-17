48cf40f0800b48b4ef49f5897c29e5f8e07a2632
Will Robertson <williamdrobertson42@gmail.com>
2022-04-17 17:08:36 +1000

    Linting

--- todos/src/components/Todo.js
+++ todos/src/components/Todo.js
@@ -1,64 +1,60 @@
-import getRandomInt from "../utils/getRandomInt"
+import getRandomInt from "../utils/getRandomInt";
 
 class Todo {
+  constructor(title, description, dueDate, id) {
+    //TODO handle absence of name and description
+    if (id !== null && id !== undefined) {
+      this.id = id;
+    } else {
+      const _ = new Date();
+      const _encoded = btoa(_.toISOString()) + getRandomInt(1, 99999);
+      this.id = _encoded;
+    }
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+  }
 
-	constructor(title, description, dueDate, id) {
-		//TODO handle absence of name and description 
-		if (id !== null && id !== undefined) {
-			this.id = id
-		} else {
-			const _ = new Date()
-			const _encoded = btoa(_.toISOString()) + getRandomInt(1, 99999)
-			this.id = _encoded
-		}
-		this.title = title;
-		this.description = description;
-		this.dueDate = dueDate;
-	}
+  getParsedDateString() {
+    const tempDate = new Date(this.dueDate);
+    let options = { weekday: "long" };
+    const locale = navigator.language;
+    const weekDay = new Intl.DateTimeFormat(locale, options).format(tempDate);
 
-	getParsedDateString() {
-		const tempDate = new Date(this.dueDate)
-		let options = { weekday: "long" }
-		const locale = navigator.language
-		const weekDay = new Intl.DateTimeFormat(locale, options).format(tempDate)
+    options = { month: "long" };
+    const month = new Intl.DateTimeFormat(locale, options).format(tempDate);
 
-		options = { month: "long" }
-		const month = new Intl.DateTimeFormat(locale, options).format(tempDate)
+    return `${weekDay} ${tempDate.getDate().toString()} ${month} ${tempDate
+      .getFullYear()
+      .toString()}`;
+  }
 
-		return `${weekDay} ${tempDate.getDate().toString()} ${month} ${tempDate.getFullYear().toString()}`
-	}
+  viewTodoDetails(e, lists) {
+    const activeListName = document.querySelector(".activeList");
+    const activeList = lists[activeListName.textContent];
+    const eventTarget = e.target;
 
-	viewTodoDetails(e, lists) {
-		const activeListName = document.querySelector(".activeList")
-		const activeList = lists[activeListName.textContent]
-		const eventTarget = e.target
-
-
-		if (eventTarget.classList.contains("todo-container")) {
-			for (const todo in activeList) {
-				if (Object.hasOwnProperty.call(activeList, todo)) {
-					const element = activeList[todo];
-					if (element.id === eventTarget.dataset.todoId) {
-						editTodo(element)
-					}
-
-				}
-			}
-		// event target may be child of the actual todo where the state lives
-		} else if (eventTarget.parentElement.classList.contains("todo-container")) {
-			for (const todo in activeList) {
-				if (Object.hasOwnProperty.call(activeList, todo)) {
-					const element = activeList[todo];
-					if (element.id === eventTarget.parentElement.dataset.todoId) {
-						editTodo(element)
-					}
-
-				}
-			}
-		}
-
-	}
-
+    if (eventTarget.classList.contains("todo-container")) {
+      for (const todo in activeList) {
+        if (Object.hasOwnProperty.call(activeList, todo)) {
+          const element = activeList[todo];
+          if (element.id === eventTarget.dataset.todoId) {
+            editTodo(element);
+          }
+        }
+      }
+      // event target may be child of the actual todo where the state lives
+    } else if (eventTarget.parentElement.classList.contains("todo-container")) {
+      for (const todo in activeList) {
+        if (Object.hasOwnProperty.call(activeList, todo)) {
+          const element = activeList[todo];
+          if (element.id === eventTarget.parentElement.dataset.todoId) {
+            editTodo(element);
+          }
+        }
+      }
+    }
+  }
 }
 
-export default Todo
+export default Todo;
--- todos/src/components/modal.js
+++ todos/src/components/modal.js
@@ -1,23 +1,23 @@
-const modal = document.querySelector("#modal")
-const modalBtn = document.querySelector(".newTodoButton")
+const modal = document.querySelector("#modal");
+const modalBtn = document.querySelector(".newTodoButton");
 
 function toggleModal() {
-	if (getComputedStyle(modal).display === "none") {
-		modal.style.display = "block"
-	} else {
-		modal.style.display = "none"
-	}
+  if (getComputedStyle(modal).display === "none") {
+    modal.style.display = "block";
+  } else {
+    modal.style.display = "none";
+  }
 }
 
 function outsideClick(e) {
-	if (e.target === modal) {
-		modal.style.display = "none"
-	}
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
 }
 
 function Modal() {
-	modalBtn.addEventListener("click", toggleModal)
-	window.addEventListener("click", outsideClick)
+  modalBtn.addEventListener("click", toggleModal);
+  window.addEventListener("click", outsideClick);
 }
 
-export default Modal
+export default Modal;
