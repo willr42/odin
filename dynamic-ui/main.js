ec02c157e7aa49378f5d067cbdc4cc6f000cc93d
Will Robertson <williamdrobertson42@gmail.com>
2022-04-29 15:22:37 +1000

    Implement exit dropdown parent feature

--- dynamic-ui/main.js
+++ dynamic-ui/main.js
@@ -1,11 +1,19 @@
 const dropdown = document.querySelector(".dropdown");
 const dropdownParent = document.querySelector(".dropdown-parent");
 const dropdownLinks = document.querySelectorAll(".dropdown-link");
+const aTagsInsideDropdown = Array.from(document.querySelectorAll(".inside-dropdown"))
 
 dropdownParent.addEventListener("mouseenter", (e) => {
-  console.log("entered dropdown");
   dropdownLinks.forEach(dropdown => dropdown.classList.add("active"));
 });
+
+dropdownParent.addEventListener("mouseleave", (e) => {
+  if(e.relatedTarget === dropdown || aTagsInsideDropdown.includes(e.relatedTarget)) { // Otherwise, when we exit towards dropdown-parent the menu does not close
+	return
+  } else {
+    dropdownLinks.forEach(dropdown => dropdown.classList.remove("active"))
+  }
+})
 
 dropdown.addEventListener("mouseleave", (e) => {
   if (e.relatedTarget === dropdownParent) {
