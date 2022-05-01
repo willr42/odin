812c6096be85bf83ae16987b95b44b2d4dad05f6
Will Robertson <williamdrobertson42@gmail.com>
2022-05-01 15:26:12 +1000

    Continue implementation

--- dynamic-ui/main.js
+++ dynamic-ui/main.js
@@ -1,24 +1,30 @@
 const dropdown = document.querySelector(".dropdown");
 const dropdownParent = document.querySelector(".dropdown-parent");
 const dropdownLinks = document.querySelectorAll(".dropdown-link");
-const aTagsInsideDropdown = Array.from(document.querySelectorAll(".inside-dropdown"))
+const anchorTagsInsideDropdown = Array.from(
+  document.querySelectorAll(".inside-dropdown")
+);
 
 dropdownParent.addEventListener("mouseenter", (e) => {
-  dropdownLinks.forEach(dropdown => dropdown.classList.add("active"));
+  dropdownLinks.forEach((dropdown) => dropdown.classList.add("active"));
 });
 
 dropdownParent.addEventListener("mouseleave", (e) => {
-  if(e.relatedTarget === dropdown || aTagsInsideDropdown.includes(e.relatedTarget)) { // Otherwise, when we exit towards dropdown-parent the menu does not close
-	return
+  if (
+    e.relatedTarget === dropdown ||
+    anchorTagsInsideDropdown.includes(e.relatedTarget)
+  ) {
+    // Otherwise, when we exit towards dropdown-parent the menu does not close
+    return;
   } else {
-    dropdownLinks.forEach(dropdown => dropdown.classList.remove("active"))
+    dropdownLinks.forEach((dropdown) => dropdown.classList.remove("active"));
   }
-})
+});
 
 dropdown.addEventListener("mouseleave", (e) => {
   if (e.relatedTarget === dropdownParent) {
     return;
   } else {
-    dropdownLinks.forEach(dropdown => dropdown.classList.remove("active"));
+    dropdownLinks.forEach((dropdown) => dropdown.classList.remove("active"));
   }
 });
